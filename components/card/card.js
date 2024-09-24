dc2e217fcd6f496b3ffae633db002e11a5fcfd70
Fadik Tekin <tekinfadik@gmail.com>
2024-09-24 15:01:37 +0200

    fetch data, pagination,search characters

--- components/card/card.js
+++ components/card/card.js
@@ -1,25 +1,26 @@
-export function createCharacterCard() {
+export function createCharacterCard(cardData) {
+  const { image, name, status, type, episode } = cardData;
+  const occurrences = episode.length;
   const cardContainer = document.querySelector('[data-js="card-container"]');
-
   const listElement = document.createElement("li");
   listElement.innerHTML = `
     <li class="card">
           <div class="card__image-container">
             <img
               class="card__image"
-              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-              alt="Rick Sanchez"
+              src=${image}
+              alt=${name}
             />
             <div class="card__image-gradient"></div>
           </div>
           <div class="card__content">
-            <h2 class="card__title">Rick Sanchez</h2>
+            <h2 class="card__title">${name}</h2>
             <dl class="card__info">
-              <dt class="card__info-title">Status</dt>
+              <dt class="card__info-title">${status}</dt>
               <dd class="card__info-description">Alive</dd>
-              <dt class="card__info-title">Type</dt>
+              <dt class="card__info-title">${type}</dt>
               <dd class="card__info-description"></dd>
-              <dt class="card__info-title">Occurrences</dt>
+              <dt class="card__info-title">${occurrences}</dt>
               <dd class="card__info-description">51</dd>
             </dl>
           </div>
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -9,6 +11,68 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
+
+async function fetchCharacters() {
+  cardContainer.innerHTML = "";
+
+  const FETCH_URL = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
+  try {
+    const response = await fetch(FETCH_URL);
+
+    if (!response.ok) {
+      console.log("API Error:", response.status);
+      return;
+    }
+
+    const data = await response.json();
+    maxPage = data.info.pages;
+    createCards(data.results);
+    updatePagination();
+  } catch (error) {
+    console.log("Error occured when fetching all characters", error);
+  }
+}
+
+function updatePagination() {
+  pagination.textContent = `${page} / ${maxPage}`;
+}
+function createCards(allCards) {
+  allCards &&
+    allCards.forEach((card) => {
+      cardContainer.append(createCharacterCard(card));
+    });
+}
+
+function onNextClick() {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+  }
+}
+
+function onPrevClick() {
+  if (page > 1) {
+    page--;
+    fetchCharacters();
+  }
+}
+
+function onSearchBarSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  searchQuery = data.query;
+  page = 1;
+
+  fetchCharacters();
+}
+
+prevButton.addEventListener("click", onPrevClick);
+nextButton.addEventListener("click", onNextClick);
+searchBar.addEventListener("submit", onSearchBarSubmit);
+
+fetchCharacters();
