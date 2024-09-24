64abf4b7e47011335891ca9a3049731531cdbe2e
Fadik Tekin <tekinfadik@gmail.com>
2024-09-24 15:05:15 +0200

    fix card info

--- components/card/card.js
+++ components/card/card.js
@@ -16,12 +16,12 @@ export function createCharacterCard(cardData) {
           <div class="card__content">
             <h2 class="card__title">${name}</h2>
             <dl class="card__info">
-              <dt class="card__info-title">${status}</dt>
-              <dd class="card__info-description">Alive</dd>
-              <dt class="card__info-title">${type}</dt>
-              <dd class="card__info-description"></dd>
-              <dt class="card__info-title">${occurrences}</dt>
-              <dd class="card__info-description">51</dd>
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${status}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${type}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${occurrences}</dd>
             </dl>
           </div>
         </li>
